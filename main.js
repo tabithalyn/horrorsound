3ec3b6e67ae68634d942db0c6427e7611e198d2c
tabithalyn <77988374+tabithalyn@users.noreply.github.com>
2022-09-06 17:50:17 -0400

    Update main.js

--- main.js
+++ main.js
@@ -3,13 +3,13 @@ var audioPlaying = false; // global scope toggle audio state on/off
 
 var playAudio = function (filePath) {
   if (audioPlaying == false) {
-    audio.src = "/Users/doom/Desktop/coding/projects/soundboard/" + filePath;
+    audio.src = "/horrorsound" + filePath;
     audioPlaying = true; 
   } else if (currentButton == this && audioPlaying == true) {
     audio.pause();
     audioPlaying = false;
   } else if (currentButton != this) {
-    audio.src = "/Users/doom/Desktop/coding/projects/soundboard/" + filePath;
+    audio.src = "/horrorsound" + filePath;
     currentButton.classList.toggle("paused") 
     audioPlaying = true; 
   }
