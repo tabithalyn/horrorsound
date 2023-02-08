313551ef1dcef7e573fbad4f1cc9c1bcf01fa212
Tabitha Lyn <tabithalyn@ymail.com>
2023-02-08 11:45:08 -0500

    Styling update

--- main.js
+++ main.js
@@ -3,13 +3,13 @@ var audioPlaying = false;
 
 var playAudio = function (filePath) {
   if (audioPlaying == false) {
-    audio.src = "github.com/tabithalyn/horrorsound/sounds" + filePath;
+    audio.src = "/Users/doom/Desktop/coding/projects/soundboard/" + filePath;
+    // github.com/tabithalyn/horrorsound/sounds
     audioPlaying = true; 
   } else if (currentButton == this && audioPlaying == true) {
     audio.pause();
     audioPlaying = false;
   } else if (currentButton != this) {
-<<<<<<< HEAD
     audio.src = "/Users/doom/Desktop/coding/projects/soundboard/" + filePath;
     currentButton.classList.toggle("paused");
     audioPlaying = true;
@@ -22,13 +22,3 @@ var playAudio = function (filePath) {
 audio.addEventListener("ended", function(){
   currentButton.classList.remove("paused");
 });
-=======
-    audio.src = "github.com/tabithalyn/horrorsound/sounds" + filePath;
-    currentButton.classList.toggle("paused") 
-    audioPlaying = true; 
-  }
-
-  currentButton = this;
-  currentButton.classList.toggle("paused")    
-} 
->>>>>>> 37d0dad1e8c94de27ccf13a56970ba2e47b0100d
